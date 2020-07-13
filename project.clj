5fb8358ced4d90005c4c22f46c50ef0db8d81902
raed <raed.awad@galilsoftware.com>
2020-07-13 12:20:27 +0300

    fix Type hint toArray for JDK11

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject hier-set "1.2.0-SNAPSHOT"
+(defproject hier-set "1.2.0-jdk11"
   :description "A Clojure hierarchical set."
   :url "http://github.com/llasram/hier-set"
   :license {:name "Eclipse Public License"
@@ -6,8 +6,8 @@
   :global-vars {*warn-on-reflection* true}
   :dependencies [[org.clojure/clojure "1.6.0"]]
   :aliases {"all" ["with-profile" ~(str "+clojure-1-4:"
-                                        "+clojure-1-5:"
-                                        "+clojure-1-6")]}
+                                    "+clojure-1-5:"
+                                    "+clojure-1-6")]}
   :profiles {:clojure-1-4 {:dependencies
                            [[org.clojure/clojure "1.4.0"]]}
              :clojure-1-5 {:dependencies
